d6031ca15b14d510d8b9ee355fbc10a67635dc3c
Stefan Luntraru <stefanluntraru@192-168-0-102.rdsnet.ro>
2021-08-26 03:22:25 +0300

    ZOOM-006: last changes

--- src/localmodules/zoom451/src/component/SliderWidget/SliderWidget.component.js
+++ src/localmodules/zoom451/src/component/SliderWidget/SliderWidget.component.js
@@ -1,11 +1,24 @@
 /* eslint-disable no-restricted-globals */
+// import Slider from 'react-slick';
+// import Slider from 'infinite-react-carousel';
 import PropTypes from 'prop-types';
 import React from 'react';
-import Slider from 'react-slick';
+// import Swiper core and required modules
+import SwiperCore, {
+    Navigation,
+    Pagination
+} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 import Html from 'Component/Html';
 import Image from 'Component/Image';
 import { SliderWidget as SrcSliderWidget } from 'SourceComponent/SliderWidget/SliderWidget.component';
+
+import 'swiper/swiper-bundle.css';
+
+// install Swiper modules
+SwiperCore.use([Pagination]);
+SwiperCore.use([Navigation]);
 
 /** @namespace Zoom451/Component/SliderWidget/Component/SliderWidgetComponent */
 export class SliderWidgetComponent extends SrcSliderWidget {
@@ -48,7 +61,7 @@ export class SliderWidgetComponent extends SrcSliderWidget {
         } = slide;
 
         return (
-            <figure
+            <SwiperSlide
               block="SliderWidget"
               elem="Figure"
               key={ i }
@@ -67,7 +80,7 @@ export class SliderWidgetComponent extends SrcSliderWidget {
                 >
                     <Html content={ slide_text || '' } />
                 </a>
-            </figure>
+            </SwiperSlide>
         );
     };
 
@@ -89,31 +102,33 @@ export class SliderWidgetComponent extends SrcSliderWidget {
     render() {
         const { slider: { slides } } = this.props;
 
-        const settings = {
-            slidesToShow: this.checkRes(),
-            slidesToScroll: 1,
-            rows: 1,
-            centerMode: false,
-            infinite: false,
-            arrows: true,
-            cssEase: 'linear'
-        };
+        // const settings = {
+        //     slidesToShow: this.checkRes(),
+        //     slidesToScroll: 1,
+        //     rows: 1,
+        //     centerMode: false,
+        //     infinite: false,
+        //     arrows: true,
+        //     cssEase: 'linear'
+        // };
 
         return (
             // eslint-disable-next-line react/jsx-no-comment-textnodes
             <>
-                { /* eslint-disable-next-line jsx-a11y/click-events-have-key-events,jsx-a11y/no-static-element-interactions */ }
-                <div
-                  /* eslint-disable-next-line react/jsx-no-bind */
-                  onClick={ () => this.slider.slickGoTo(0) }
-                  className="SliderWidget-JumpFirst"
-                >
-                    { __('Jump to first slide') }
-                </div>
                 { /* eslint-disable-next-line @scandipwa/scandipwa-guidelines/jsx-no-props-destruction,no-return-assign */ }
-                <Slider ref={ (slider) => (this.slider = slider) } { ...settings }>
+                <div className="navigationPrev" />
+                <Swiper
+                  modules={ [Pagination, Navigation] }
+                  slidesPerView={ this.checkRes() }
+                  spaceBetween={ 20 }
+                  navigation={ {
+                      prevEl: '.navigationPrev',
+                      nextEl: '.navigationNext'
+                  } }
+                >
                     { slides.map(this.renderSlide) }
-                </Slider>
+                </Swiper>
+                <div className="navigationNext" />
             </>
         );
     }
